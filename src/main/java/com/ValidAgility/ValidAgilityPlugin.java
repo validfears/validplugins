72d45d29e898ea03c8a3ca7d8877c8580760628b
Valid <scott.d.fletcher.1@gmail.com>
2022-07-21 00:40:25 -0600

    trying

--- src/main/java/com/ValidAgility/ValidAgilityPlugin.java
+++ src/main/java/com/ValidAgility/ValidAgilityPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ValidAgility;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "ValidAgility"
 )
-public class ExamplePlugin extends Plugin
+public class ValidAgilityPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ValidAgilityConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ValidAgilityConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ValidAgilityConfig.class);
 	}
 }
--- src/test/java/com/ValidAgility/ValidAgilityTest.java
+++ src/test/java/com/ValidAgility/ValidAgilityTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.ValidAgility;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ValidAgilityTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ValidAgilityPlugin.class);
 		RuneLite.main(args);
 	}
 }
